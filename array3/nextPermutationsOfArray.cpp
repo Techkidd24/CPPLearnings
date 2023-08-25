00c56269fb20e80b798ab073ab1d6b575908de2b
Techkidd <ronakverma2131@gmail.com>
2023-08-25 21:01:53 +0530

    Modified permutationsOfArray

--- array3/nextPermutationsOfArray.cpp
+++ array3/nextPermutationsOfArray.cpp
@@ -13,7 +13,7 @@ int main(){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
-    int idx=-1; //pivot index
+    int idx=-1; //1)pivot index
     for(int i=arr.size()-2; i>=0; i--){
         if(arr[i]<arr[i+1]){
             idx=i;
@@ -23,12 +23,21 @@ int main(){
     if(idx==-1){
         reverse(arr.begin(), arr.end()); //if there is no next permutation and array is already greatest
     }
-    //sorting/reversing after pivot
+    //2)sorting/reversing after pivot
     reverse(arr.begin()+idx+1, arr.end());
-    //swapping idx and idx+1
+    //3)finding number just greater than pivot
+    int j=0;
+    for(int i=idx+1; i<arr.size(); i++){
+        if(arr[i]>arr[idx]){
+            j=i;
+            break;
+        }
+    }
+
+    //swapping idx and j
     int temp=arr[idx];
-    arr[idx]=arr[idx+1];
-    arr[idx+1]=temp;
+    arr[idx]=arr[j];
+    arr[j]=temp;
 
     for(int i=0; i<arr.size(); i++){
         cout<<arr[i]<<" ";

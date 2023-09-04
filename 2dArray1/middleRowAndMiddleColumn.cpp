bbed960451669addbf35cea96ca888b4ddcbce2d
Techkidd <ronakverma2131@gmail.com>
2023-09-04 23:04:44 +0530

    displays elements of middle row and middle column

---
+++ 2dArray1/middleRowAndMiddleColumn.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of row: ";
+    cin>>n;
+    int arr[n][n];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==n/2 || j==n/2)    cout<<arr[i][j]<<" ";
+            else    cout<<" ";
+        }
+        cout<<endl;
+    }
+}

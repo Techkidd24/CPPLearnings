44b5f62917e0cdd6a6059a63261689ab9e474c8b
Techkidd <ronakverma2131@gmail.com>
2023-09-07 21:42:16 +0530

    prints elements present in diagonals

---
+++ 2dArray2/printDiagonalElements.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m,n;
+    cout<<"Enter rows in matrix: ";
+    cin>>m;
+    cout<<endl;
+    cout<<"Enter columns in matrix: ";
+    cin>>n;
+    cout<<endl;
+    int arr[m][n];
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            if(i==j || i+j==m-1)  cout<<arr[i][j];
+            else    cout<<" ";
+        }
+        cout<<endl;
+    }
+}

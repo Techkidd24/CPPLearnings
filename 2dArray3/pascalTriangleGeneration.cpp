99f1cb8ecf2b53eeab3e6d66a34e3d9b7180cb4e
Techkidd <ronakverma2131@gmail.com>
2023-09-12 22:22:09 +0530

    Pascal Triangle using 2d vector

---
+++ 2dArray3/pascalTriangleGeneration.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter no. of rows: ";
+    cin>>m;
+    vector< vector<int> > v;
+    for(int i=1; i<=5; i++){
+        vector<int> a(i);
+        v.push_back(a);
+    }
+    
+    //generate
+    for(int i=0; i<m; i++){
+        for(int j=0; j<=i; j++){
+            if(j==0 || j==i)    v[i][j]=1;
+            else    v[i][j]=v[i-1][j-1]+v[i-1][j];
+        }
+    }
+    //print
+    for(int i=0; i<m; i++){
+        for(int j=0; j<=i; j++){
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
